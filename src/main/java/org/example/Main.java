ea33310631221190ba8855ab326a23025227a26d
Michael O'Brien <obrien.michael@outlook.ie>
2024-04-09 03:29:07 +0100

    Updated Main

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,17 @@
 package org.example;
 
+/**
+ * This class demonstrates a simple "Hello, World!" program.
+ */
 public class Main {
+
+  /**
+   * This method prints "Hello, World!" to the console.
+   *
+   * @param args The command line arguments (not used).
+   *
+   */
   public static void main(String[] args) {
-    System.out.println("Hello world!");
+    System.out.println("Hello, World!");
   }
 }
